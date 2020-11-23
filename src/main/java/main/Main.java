a7ce833ab47ac3e3512f92264ee9153d0fbb2c32
SilentWatcher45 <ovidiu.adrian92@gmail.com>
2020-11-23 19:05:25 +0200

    Added several services.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,15 @@
 package main;
 
+import services.ConsoleService;
+import services.MechanicsService;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		ConsoleService console = new ConsoleService();
+		MechanicsService mechanics = new MechanicsService();
+		
+		
 	}
 
 }
